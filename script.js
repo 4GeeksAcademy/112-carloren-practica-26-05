3c538d4c9eeedfc0d4017e58a8982121cf9a2a00
Carlos Lorenzo <carloren.1996@gmail.com>
2025-05-26 11:07:59 +0000

    ejercicios de rosinni hasta el 2.3

---
+++ script.js
@@ -0,0 +1,49 @@
+// EJERCICIO 1.2 ----------------------------
+
+// let nombre = prompt("Escribe tu nombre")
+
+// let apellido = prompt("Escribe tu apellido")
+
+// console.log(`${nombre} ${apellido}`);
+
+// alert(`Bienvenido a nuestra página, ${nombre} ${apellido}`)
+
+// EJERCICIO 1.3 ------------------------------
+
+// let num1 = parseInt(prompt("Numero 1"))
+
+// let num2 = parseInt(prompt("Numero 2"))
+
+// let result = num1 + num2;
+
+// alert(`El resultado es: ${result}`)
+
+// EJERCICIO 1.4 -----------------------------------
+
+// let texto = prompt("Escribe tu texto")
+
+// alert(`El largo de tu texto es ${texto.length}`)
+
+// EJERCICIO 2.1 -------------------------------------
+
+// let ensaladaDeFrutas = ["pera", "manzana", "platano", "naranja", "fresa", "melocoton"]
+
+// console.log("Las frutas que hay en la ensalada son:");
+
+// for (let i = 0; i < ensaladaDeFrutas.length; i++) {
+//     console.log(ensaladaDeFrutas[i]);
+// }
+
+// EJERCICIO 2.2 -------------------------------------
+
+let edades = [8, 16, 25, 13, 15, 10]
+
+let media = 0
+
+for (let i = 0; i < edades.length; i++) {
+    media += edades[i] / edades.length
+}
+
+console.log(media);
+
+// EJERCICIO 2.3 -------------------------------------
