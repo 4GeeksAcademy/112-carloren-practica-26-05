f60091815f27d95d83cca770381dc1a4eeb93ed1
Carlos Lorenzo <carloren.1996@gmail.com>
2025-06-16 09:15:02 +0000

    16-06

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,34 +7,39 @@ let clicked = false;
 
 //create your first component
 const Home = () => {
-	const [nombre, setNombre] = useState("Luis")
-	const [text, setText] = useState("Cambiar nombre")
-	const [color, setColor] = useState("btn-warning")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
 
+	function handleChange(event) {
+		setEmail(event.target.value);
+		console.log(email);
+	}
 
-	console.log(nombre);
-
-	function saludar() {
-		console.log(clicked);
-
-		if (clicked === false) {
-			clicked = true
-			setNombre("Carlos")
-			setText("Nombre cambiado")
-			setColor("btn-success")
-		} else {
-			clicked = false
-			setNombre("Luis")
-			setText("Cambiar nombre")
-			setColor("btn-warning")
-		}
-		console.log(clicked);
+	function enviarDatos(event) {
+		event.preventDefault()
+		console.log(email, password);
 
 	}
+
 	return (
 		<div className="text-center">
-			<h1>Hola {nombre}</h1>
-			<p className={`btn ${color}`} onClick={saludar}>{text}</p>
+			<h1>Componentes React controlados</h1>
+			<form className="w-50 mx-auto align-self-center" onSubmit={enviarDatos}>
+				<div className="form-group">
+					<label htmlFor="exampleInputEmail1">Email address</label>
+					<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(event) => setEmail(event.target.value)} placeholder="Enter email" />
+					<small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+				</div>
+				<div className="form-group">
+					<label htmlFor="exampleInputPassword1">Password</label>
+					<input type="password" className="form-control" id="exampleInputPassword1" onChange={(event) => setPassword(event.target.value)} placeholder="Password" />
+				</div>
+				<div className="form-group form-check">
+					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+					<label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+				</div>
+				<button type="submit" className="btn btn-primary">Submit</button>
+			</form>
 		</div>
 	);
 };
