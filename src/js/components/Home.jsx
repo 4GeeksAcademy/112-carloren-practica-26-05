1530e7dd96895744c78abb824ecf02cba8388759
Carlos Lorenzo <carloren.1996@gmail.com>
2025-06-18 09:28:50 +0000

    Esqueleto lista

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -8,25 +8,15 @@ let clicked = false;
 //create your first component
 const Home = () => {
 
-	function handleChange(event) {
-		setEmail(event.target.value);
-		console.log(email);
-	}
-
-	function enviarDatos(event) {
-		event.preventDefault()
-		console.log(email, password);
-
-	}
 
 	return (
 		<div className="d-flex flex-column text-center">
 			<h1>To-Do List</h1>
 			<ul id="todo" className="list-group border align-self-center m-0">
-				<li className="list-group-item align-self-center p-0" style={{ width: "20em" }}><input type="text" className="m-0 p-2 w-100" placeholder="Añadir tarea" /></li>
+				<li className="list-group-item align-self-center" style={{ width: "20em" }}><input type="text" className="border border-0 m-0 p-2 w-100" placeholder="Añadir tarea" /></li>
 			</ul>
-			<p className="border bg-body-tertiary align-self-center mb-0" style={{ width: "19.5em", height: "5px", marginTop: "-1px" }}></p>
-			<p className="border bg-light align-self-center" style={{ width: "19em", height: "5px", marginTop: "-1px" }}></p>
+			<p className="border bg-secondary align-self-center mb-0" style={{ width: "19.5em", height: "5px", marginTop: "-1px" }}></p>
+			<p className="border bg-dark align-self-center" style={{ width: "19em", height: "5px", marginTop: "-1px" }}></p>
 		</div>
 	);
 };
