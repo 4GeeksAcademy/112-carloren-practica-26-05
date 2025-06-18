483c1451b2caf0e2c70c600e3f42412503822fb9
Carlos Lorenzo <carloren.1996@gmail.com>
2025-06-18 08:45:52 +0000

    esqueleto lista

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,8 +7,6 @@ let clicked = false;
 
 //create your first component
 const Home = () => {
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
 
 	function handleChange(event) {
 		setEmail(event.target.value);
@@ -22,24 +20,13 @@ const Home = () => {
 	}
 
 	return (
-		<div className="text-center">
-			<h1>Componentes React controlados</h1>
-			<form className="w-50 mx-auto align-self-center" onSubmit={enviarDatos}>
-				<div className="form-group">
-					<label htmlFor="exampleInputEmail1">Email address</label>
-					<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(event) => setEmail(event.target.value)} placeholder="Enter email" />
-					<small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-				</div>
-				<div className="form-group">
-					<label htmlFor="exampleInputPassword1">Password</label>
-					<input type="password" className="form-control" id="exampleInputPassword1" onChange={(event) => setPassword(event.target.value)} placeholder="Password" />
-				</div>
-				<div className="form-group form-check">
-					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
-					<label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-				</div>
-				<button type="submit" className="btn btn-primary">Submit</button>
-			</form>
+		<div className="d-flex flex-column text-center">
+			<h1>To-Do List</h1>
+			<ul id="todo" className="list-group border align-self-center m-0">
+				<li className="list-group-item align-self-center p-0" style={{ width: "20em" }}><input type="text" className="m-0 p-2 w-100" placeholder="Añadir tarea" /></li>
+			</ul>
+			<p className="border bg-body-tertiary align-self-center mb-0" style={{ width: "19.5em", height: "5px", marginTop: "-1px" }}></p>
+			<p className="border bg-light align-self-center" style={{ width: "19em", height: "5px", marginTop: "-1px" }}></p>
 		</div>
 	);
 };
